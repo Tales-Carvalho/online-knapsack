5235ad07abd541c466e8e0c401399ba83b8da7ab
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-12-04 22:30:14 -0700

    Adjusted tolerance for numerical computations

--- utils.py
+++ utils.py
@@ -8,6 +8,6 @@ def get_values(items: list[Item], variables: list[int]):
     sum_weights = np.sum([i.weight * x for i, x in zip(items, variables)])
     return sum_values, sum_weights
 
-def verify_result(sum_values: float, sum_weights: float, objective: float, tol: float = 0.00001):
+def verify_result(sum_values: float, sum_weights: float, objective: float, tol: float = 0.01):
     assert(sum_weights < 1 + tol)
     assert(np.isclose(sum_values, objective, tol))
