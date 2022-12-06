ef157e20a5f23691ce2d9a286355b5e43c8c881d
Tales Carvalho <tales-h-carvalho@hotmail.com>
2022-12-05 20:13:15 -0700

    Fixed OKP in multidimensional setting

--- online_knapsack.py
+++ online_knapsack.py
@@ -11,17 +11,17 @@ def threshold(y: float, p_min: float, p_max: float, beta_star: float):
 
 def online_knapsack(items: list[Item], p_min: float, p_max: float):
     beta_star = 1/(1 + np.log(p_max/p_min))
-    y = [0 for _ in range(len(items[0].weights))]
+    y = np.zeros((len(items[0].weights),))
     objective_value = 0
     decision_variables = []
     for i, item in enumerate(items):
         x = 1
         for j, w in enumerate(item.weights):
-            p = threshold(y[j], p_min, p_max, beta_star)
-            if item.value / item.weight < p:
+            p = threshold(y[j] + w, p_min, p_max, beta_star)
+            if item.value / w < p:
                 x = 0
         if x == 1:
-            y += item.weights # TODO: does this work?
+            y += np.array(item.weights)
             objective_value += item.value
         decision_variables.append(x)
     return objective_value, decision_variables
